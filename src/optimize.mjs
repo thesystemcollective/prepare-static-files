1eca341626588d7848d1206e217d788b9aefbf17
jascha <jascha@jaeh.at>
2020-10-23 20:23:50 +0200

    optimize: format

--- src/optimize.mjs
+++ src/optimize.mjs
@@ -11,50 +11,51 @@ import sharp from 'sharp'
 import { isImage } from './lib.mjs'
 
 export const optimize = async ({ files, silent, noWebp }) => {
-  await Promise.all(files.map(async file => {
-    if (isImage(file)) {
-      const sharpen = sharp(file)
+  await Promise.all(
+    files.map(async file => {
+      if (isImage(file)) {
+        const sharpen = sharp(file)
 
-      if (is.undefined(noWebp)) {
-        const extension = path.extname(file)
-        const webpName = file.replace(extension, '.webp')
-        const webpExists = await fs.exists(webpName)
-        if (!webpExists) {
+        if (is.undefined(noWebp)) {
+          const extension = path.extname(file)
+          const webpName = file.replace(extension, '.webp')
+          const webpExists = await fs.exists(webpName)
+          if (!webpExists) {
+            if (!silent) {
+              log.info('generate webp', webpName)
+            }
+
+            const sharpen = sharp(file)
+            await sharpen.toFile(webpName)
+          }
+        }
+
+        if (file.endsWith('jpg')) {
+          const data = await sharpen.jpeg({
+            trellisQuanttisation: true,
+            quality: 80,
+            optimizeCoding: true,
+          })
+
+          const buffer = await data.toBuffer()
+
           if (!silent) {
-            log.info('generate webp', webpName)
+            log.info('overwrite', file)
           }
 
-          const sharpen = sharp(file)
-          await sharpen.toFile(webpName)
+          await fs.writeFile(file, buffer)
+        } else if (file.endsWith('png')) {
+          const data = await sharpen.png()
+
+          const buffer = await data.toBuffer()
+
+          if (!silent) {
+            log.info('overwrite', file)
+          }
+
+          await fs.writeFile(file, buffer)
         }
       }
-
-      if (file.endsWith('jpg')) {
-        const data = await sharpen.jpeg({
-          trellisQuanttisation: true,
-          quality: 80,
-          optimizeCoding: true,
-        })
-
-        const buffer = await data.toBuffer()
-
-        if (!silent) {
-          log.info('overwrite', file)
-        }
-
-        await fs.writeFile(file, buffer)
-
-      } else if (file.endsWith('png')) {
-        const data = await sharpen.png()
-
-        const buffer = await data.toBuffer()
-
-        if (!silent) {
-          log.info('overwrite', file)
-        }
-
-        await fs.writeFile(file, buffer)
-      }
-    }
-  }))
+    }),
+  )
 }

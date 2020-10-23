1b97ed529bdeb07f5a99ffccba49cc5c6f27c865
jascha <jascha@jaeh.at>
2020-10-24 01:00:32 +0200

    replace isMp3 with isLossLess

--- src/lib.mjs
+++ src/lib.mjs
@@ -1,9 +1,11 @@
 export const isImage = file =>
-  file.endsWith('png') || file.endsWith('jpg') || file.endsWith('webp') || file.endsWith('ico')
+  file.endsWith('.png') || file.endsWith('.jpg') || file.endsWith('.webp') || file.endsWith('.ico')
 
-export const isVideo = file => file.endsWith('mp4') || file.endsWith('webm')
-export const isAudio = file => file.endsWith('mp3') || file.endsWith('m4a') || file.endsWith('ogg')
-export const isMp3 = file => file.endsWith('mp3')
+export const isVideo = file => file.endsWith('.mp4') || file.endsWith('.webm')
+export const isAudio = file => file.endsWith('.mp3') || file.endsWith('.m4a') || file.endsWith('.ogg')
+export const isLossLess = file => file.endsWith('.flac') || file.endsWith('.wav') || file.endsWith('.alac')
+
+export const isZip = file => file.endsWith('.gz')
 
 export const isCompressible = file =>
-  !isImage(file) && !isVideo(file) && !isAudio(file) && !file.endsWith('.gz')
+  !isImage(file) && !isVideo(file) && !isAudio(file) && !isLossLess(file) && !isZip(file)

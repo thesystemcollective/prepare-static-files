763cfa2e7d9ab292f0686e92107ef4a288beac2c
jascha <jascha@jaeh.at>
2020-10-24 00:56:00 +0200

    lib: isAudio detects all audio files, isMp3 added for mp3 detection

--- src/lib.mjs
+++ src/lib.mjs
@@ -2,7 +2,8 @@ export const isImage = file =>
   file.endsWith('png') || file.endsWith('jpg') || file.endsWith('webp') || file.endsWith('ico')
 
 export const isVideo = file => file.endsWith('mp4') || file.endsWith('webm')
-export const isAudio = file => file.endsWith('mp3')
+export const isAudio = file => file.endsWith('mp3') || file.endsWith('m4a') || file.endsWith('ogg')
+export const isMp3 = file => file.endsWith('mp3')
 
 export const isCompressible = file =>
   !isImage(file) && !isVideo(file) && !isAudio(file) && !file.endsWith('.gz')

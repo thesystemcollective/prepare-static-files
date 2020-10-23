dfd1d855e6dcd0a988e17f2011566c74b575d04a
jascha <jascha@jaeh.at>
2020-10-23 20:23:36 +0200

    src/compress.mjs: add compress functionality

--- src/compress.mjs
+++ src/compress.mjs
@@ -1,12 +1,41 @@
 #!/usr/bin/env node
 
 import log from '@magic/log'
+import fs from '@magic/fs'
+
+import zopfli from 'node-zopfli-es'
 
 import { isCompressible } from './lib.mjs'
 
 export const compress = async ({ files, silent }) =>
-  await Promise.all(files.map(async file => {
-    if (isCompressible(file)) {
-      console.log('compressible', file)
-    }
-  }))
+  await Promise.all(
+    files.map(async file => {
+      if (isCompressible(file)) {
+        const outputName = `${file}.gz`
+
+        const exists = await fs.exists(outputName)
+
+        if (exists) {
+          return
+        }
+
+        const input = await fs.readFile(file)
+
+        const options = {
+          verbose: false,
+          verbose_more: false,
+          numiterations: 15,
+          blocksplitting: true,
+          blocksplittinglast: false,
+          blocksplittingmax: 15,
+        }
+
+        const zipped = await zopfli.gzip(input, options)
+        if (zipped.length < input.length) {
+          await fs.writeFile(outputName, zipped)
+        } else {
+          log.warn('ZIP', 'zipped file is bigger than original, did not save compressed file', file)
+        }
+      }
+    }),
+  )

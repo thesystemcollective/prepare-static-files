5a08d1a1d674cae37d2ba6e21650cfbd0f27518b
jascha <jascha@jaeh.at>
2020-10-24 01:12:47 +0200

    audio: convert flac to mp3, ogg, m4a

--- src/audio.mjs
+++ src/audio.mjs
@@ -1,9 +1,49 @@
+import path from 'path'
+
+import fs from '@magic/fs'
+import log from '@magic/log'
+
 import ffmpeg from 'fluent-ffmpeg'
-import { isAudio } from './lib.mjs'
+
+import { isLossLess } from './lib.mjs'
+
+// ffmpeg -i input.mp3 -c:a aac -b:a 192k output.m4a
+
+const convertFile = ({ file, codec, ext }) => new Promise(async res => {
+  const origExt = path.extname(file)
+  const name = file.replace(origExt, ext)
+
+  const exists = await fs.exists(name)
+  if (exists) {
+    res()
+    return
+  }
+
+  ffmpeg(file)
+    .addOutput(name)
+    .audioBitrate(128)
+    .audioCodec(codec)
+    .on('end', res)
+    .run()
+})
 
 export const audio = async ({ files, silent }) =>
-  await Promise.all(files.map(async file => {
-    if (isAudio(file)) {
-      console.log('is audio', file)
-    }
-  }))
+  await Promise.all(
+    files.map(async file => {
+      if (isLossLess(file)) {
+        if (!silent) {
+          log.info('start converting audio:', file)
+        }
+
+        await Promise.all([
+          convertFile({ file, codec: 'aac', ext: '.m4a' }),
+          convertFile({ file, codec: 'libvorbis', ext: '.ogg' }),
+          convertFile({ file, codec: 'libmp3lame', ext: '.mp3' }),
+        ])
+
+        if (!silent) {
+          log.info('wrote converted audio files for:', file)
+        }
+      }
+    })
+  )

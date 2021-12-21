27a0863626efca5a1a30bdac932fd8ac78ae09ab
jascha <j@jaeh.at>
2021-12-21 23:44:49 +0100

    src/etags: handle non array dirs input, sort csv entries alphabetically

--- src/etags.mjs
+++ src/etags.mjs
@@ -1,30 +1,41 @@
 import crypto from 'crypto'
 import path from 'path'
 
+import is from '@magic/types'
 import fs from '@magic/fs'
 
-export const etags = async dirs =>
-  await Promise.all(
+export const etags = async dirs => {
+  if (is.string(dirs)) {
+    dirs = [ dirs ]
+  }
+
+  dirs = dirs.sort((a, b) => a - b)
+
+  return await Promise.all(
     dirs.map(async dir => {
       const outFile = path.join(dir, 'etags.csv')
-      const lines = []
 
       const files = await fs.getFiles(dir)
 
-      await Promise.all(
-        files.map(async file => {
+      const sorted = files.sort()
+
+      const lines = await Promise.all(
+        sorted.map(async file => {
           const content = await fs.readFile(file)
           const fileWithoutDir = file.replace(`${dir}/`, '')
           const etag = await crypto.createHash('sha1').update(content).digest('base64')
 
           // do not etag the etags.csv file and ignore gzipped files
           if (file !== outFile && !file.endsWith('.gz')) {
-            lines.push(`${fileWithoutDir},${etag}`)
+            return `${fileWithoutDir},${etag}`
           }
         }),
       )
 
-      const csv = lines.join('\n')
+      const csv = lines.filter(a => a).join('\n')
       await fs.writeFile(outFile, csv)
+
+      return csv
     }),
   )
+}
